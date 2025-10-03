85507e89e315bfca8ed987840a6e75bf09f78ab1
Mr. Soros Photchara <photcharasoros1@gmail.com>
2025-10-03 17:14:46 +0700

    add detail

---
+++ Views/DetailHost/DetailHost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MonkeyAssenbly.Views.DetailHost
+{
+    public class DetailHostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
